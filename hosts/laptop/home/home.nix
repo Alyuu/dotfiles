6412600ec3caa8c96b4f272351b343af99707eb8
Alyu NixOS <alejandro.yusesc@gmail.com>
2024-03-01 13:52:49 +0100

    Reapply "Prueba hypr"
    This reverts commit 0e189f1cbc8d1c44fc11a6226610d872cf09d166.

--- hosts/laptop/home/home.nix
+++ hosts/laptop/home/home.nix
@@ -4,69 +4,61 @@
 	imports = [
 		./conf
 	];
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "alyu";
-  home.homeDirectory = "/home/alyu";
+	
+	home = {
+		username = "alyu";
+		homeDirectory = "/home/alyu";
+	};
 
-  programs.git = {
-  	userName = "Alyu NixOs";
-	userEmail = "alejandro.yusesc@gmail.com";
-  };
+	home.packages = with pkgs; [
+		alacritty
+		neovim
+		floorp
+		wofi
+		git
+	];
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "23.11"; # Please read the comment before changing.
+	programs.git = {
+		userName = "Alyu NixOs";
+		userEmail = "alejandro.yusesc@gmail.com";
+	};
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = with pkgs; [
-	alacritty
-	neovim
-	floorp
-	wofi
-	git
-  ];
+	# Home Manager is pretty good at managing dotfiles. The primary way to manage
+	# plain files is through 'home.file'.
+	home.file = {
+		# # Building this configuration will create a copy of 'dotfiles/screenrc' in
+		# # the Nix store. Activating the configuration will then make '~/.screenrc' a
+		# # symlink to the Nix store copy.
+		# ".screenrc".source = dotfiles/screenrc;
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+		# # You can also set the file content immediately.
+		# ".gradle/gradle.properties".text = ''
+		#   org.gradle.console=verbose
+		#   org.gradle.daemon.idletimeout=3600000
+		# '';
+	};
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
+	# Home Manager can also manage your environment variables through
+	# 'home.sessionVariables'. If you don't want to manage your shell through Home
+	# Manager then you have to manually source 'hm-session-vars.sh' located at
+	# either
+	#
+	#  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+	#
+	# or
+	#
+	#  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+	#
+	# or
+	#
+	#  /etc/profiles/per-user/alyu/etc/profile.d/hm-session-vars.sh
+	#
+	home.sessionVariables = {
+		# EDITOR = "emacs";
+	};
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/alyu/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
+	# Let Home Manager install and manage itself.
+	programs.home-manager.enable = true;
 
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+	home.stateVersion = "23.11";
 }
