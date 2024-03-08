c8dbbc745227f3741f8a285b1b25ccecd766f49b
Alyu NixOS <alejandro.yusesc@gmail.com>
2024-03-08 22:21:40 +0100

    ChatGPT is amazing

--- hosts/laptop/home/conf/neovim/default.nix
+++ hosts/laptop/home/conf/neovim/default.nix
@@ -8,50 +8,57 @@
 		vimAlias = true;
 		vimdiffAlias = true;
 
-		#extraLuaConfig = ''
-		#	${builtins.readFile ./config/init.lua}
+        preBuild = ''
+            mkdir -p $out/home/$USER/.config/nvim
+            cp -r ${toString ./config}/* $out/home/$USER/.config/nvim
+        '';
+
+        #extraLuaConfig = ''
+        #    lua << EOF
+        #        require ("alyu")
+        #    EOF
 		#'';
 		
-		extraLuaConfig = ''
-			${builtins.readFile ./config/options.lua}
-			${builtins.readFile ./config/keymaps.lua}
-		'';
+		# extraLuaConfig = ''
+		# 	${builtins.readFile ./config/options.lua}
+		#	${builtins.readFile ./config/keymaps.lua}
+		#'';
 
-		plugins = with pkgs.vimPlugins; [
-			# theme
-			{
-				plugin = gruvbox-nvim;
-				config = "colorscheme gruvbox";
-			}
-		
-			# line
-			lualine-nvim
-			
-			# file manager
-			oil-nvim
-		
-			# telescope
-			plenary-nvim
-			{
-				plugin = telescope-nvim;
-                type = "lua";
-				config = ./config/plugins/telescope.lua;
-			}
-			telescope-fzf-native-nvim
-			
-			# treesitter
-			{
-				plugin = (nvim-treesitter.withPlugins (p: [
-					p.tree-sitter-nix
-					p.tree-sitter-vim
-					p.tree-sitter-bash
-					p.tree-sitter-lua
-					p.tree-sitter-python
-					p.tree-sitter-json
-				]));
-                type = "lua";
-				config = ./config/plugins/treesitter.lua;
-			}
-		];
+		#plugins = with pkgs.vimPlugins; [
+		#	# theme
+		#	{
+		#		plugin = gruvbox-nvim;
+		#		config = "colorscheme gruvbox";
+		#	}
+		#
+		#	# line
+		#	lualine-nvim
+		#	
+		#	# file manager
+		#	oil-nvim
+		#
+		#	# telescope
+		#	plenary-nvim
+		#	{
+		#		plugin = telescope-nvim;
+        #        type = "lua";
+		#		config = ./config/plugins/telescope.lua;
+		#	}
+		#	telescope-fzf-native-nvim
+		#	
+		#	# treesitter
+		#	{
+		#		plugin = (nvim-treesitter.withPlugins (p: [
+		#			p.tree-sitter-nix
+		#			p.tree-sitter-vim
+		#			p.tree-sitter-bash
+		#			p.tree-sitter-lua
+		#			p.tree-sitter-python
+		#			p.tree-sitter-json
+		#		]));
+        #        type = "lua";
+		#		config = ./config/plugins/treesitter.lua;
+		#	}
+		#];
 	};
 }
