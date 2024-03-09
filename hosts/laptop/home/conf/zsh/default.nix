b93d82056d5b8f4c76b52c781a8cc25c519a2410
Alyu NixOS <alejandro.yusesc@gmail.com>
2024-03-09 13:04:54 +0100

    have to do this to generate p10k

--- hosts/laptop/home/conf/zsh/default.nix
+++ hosts/laptop/home/conf/zsh/default.nix
@@ -1,10 +1,10 @@
 { pkgs, config, lib, ... }:
 
 {
-    home.file."zsh-config" = {
-        source = ./config;
-        target = ".config/zsh";
-    };
+    #home.file."zsh-config" = {
+    #    source = ./config;
+    #    target = ".config/zsh";
+    #};
 
     home.file.".zshenv" = {
         text = "ZDOTDIR=$HOME/.config/zsh";
