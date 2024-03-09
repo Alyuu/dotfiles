bb8c7454adf17e1bf5f2433d2e0e024a23ec47bf
Alyu NixOS <alejandro.yusesc@gmail.com>
2024-03-09 15:37:47 +0100

    aaaa

--- hosts/laptop/home/conf/zsh/default.nix
+++ hosts/laptop/home/conf/zsh/default.nix
@@ -14,10 +14,6 @@
 
     # Load plugins
     home.file = {
-        ".p10k.zsh" = {
-            source = ./config/.p10k.zsh;
-            executable = true;
-        };
         ".local/share/zsh/zsh-autosuggestions".source = "${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions";
         ".local/share/zsh/zsh-fast-syntax-highlighting".source = "${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions";
         #".local/share/zsh/nix-zsh-completions".source = "${pkgs.nix-zsh-completions}/share/zsh/plugins/nix";
