a359986e8b6ce69526195ea722cfe3b0ead9edd2
Alyu NixOS <alejandro.yusesc@gmail.com>
2024-03-12 18:55:00 +0100

    The before

--- hosts/laptop/home/conf/neovim/config/lua/alyu/lazy/lsp.lua
+++ hosts/laptop/home/conf/neovim/config/lua/alyu/lazy/lsp.lua
@@ -1,21 +1,55 @@
 return {
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
-        lazy = true,
-        config = false,
-    },
-    {
-        'neovim/nvim-lspconfig',
-        dependencies = {
-            {'hrsh7th/cmp-nvim-lsp'},
-        }
-    },
     -- Autocompletion
     {
         'hrsh7th/nvim-cmp',
         dependencies = {
             {'L3MON4D3/LuaSnip'}
         },
+    },
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        lazy = true,
+        config = function()
+            local lsp_zero = require('lsp-zero')
+
+            lsp_zero.on_attach(function(client, bufnr)
+                lsp_zero.default_keymaps({buffer = bufnr})
+            end)
+        end
+    },
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            {'hrsh7th/cmp-nvim-lsp'},
+        },
+        config = function()
+            require('lspconfig').lua_ls.setup({
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = { 'vim' }
+                        },
+                        runtime = {
+                            version = 'LuaJIT',
+                        },
+                        telemetry = {
+                            enable = false,
+                        },
+                        workspace = {
+                            library = vim.api.nvim_get_runtime_file("", true),
+                        },
+                    },
+                }
+            })
+            require('lspconfig').nil_ls.setup({
+                settings = {
+                    ['nil'] = {
+                        formatting = { command = { "alejandra" } },
+                    },
+                }
+            })
+            require('lspconfig').rust_analyzer.setup({})
+        end
     },
 }
