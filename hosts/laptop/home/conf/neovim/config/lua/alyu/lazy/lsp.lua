9afe2614d391554764962c7f33d40ad61a3d9951
Alyu NixOS <alejandro.yusesc@gmail.com>
2024-03-15 19:23:06 +0100

    Added hop to nvim

--- hosts/laptop/home/conf/neovim/config/lua/alyu/lazy/lsp.lua
+++ hosts/laptop/home/conf/neovim/config/lua/alyu/lazy/lsp.lua
@@ -1,55 +1,22 @@
 return {
-    -- Autocompletion
-    {
-        'hrsh7th/nvim-cmp',
-        dependencies = {
-            {'L3MON4D3/LuaSnip'}
-        },
+    "dundalek/lazy-lsp.nvim",
+    dependencies = {
+        "neovim/nvim-lspconfig",
+        {"VonHeikemen/lsp-zero.nvim", branch = "v3.x"},
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/nvim-cmp",
     },
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
-        lazy = true,
-        config = function()
-            local lsp_zero = require('lsp-zero')
+    config = function()
+        local lsp_zero = require("lsp-zero")
 
-            lsp_zero.on_attach(function(client, bufnr)
-                lsp_zero.default_keymaps({buffer = bufnr})
-            end)
-        end
-    },
-    {
-        'neovim/nvim-lspconfig',
-        dependencies = {
-            {'hrsh7th/cmp-nvim-lsp'},
-        },
-        config = function()
-            require('lspconfig').lua_ls.setup({
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            globals = { 'vim' }
-                        },
-                        runtime = {
-                            version = 'LuaJIT',
-                        },
-                        telemetry = {
-                            enable = false,
-                        },
-                        workspace = {
-                            library = vim.api.nvim_get_runtime_file("", true),
-                        },
-                    },
-                }
+        lsp_zero.on_attach(function(client, bufnr)
+            -- see :help lsp-zero-keybindings to learn the available actions
+            lsp_zero.default_keymaps({
+                buffer = bufnr,
+                preserve_mappings = false
             })
-            require('lspconfig').nil_ls.setup({
-                settings = {
-                    ['nil'] = {
-                        formatting = { command = { "alejandra" } },
-                    },
-                }
-            })
-            require('lspconfig').rust_analyzer.setup({})
-        end
-    },
+        end)
+
+        require("lazy-lsp").setup {}
+    end,
 }
