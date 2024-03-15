33497c5293b7f04f8949278b2d674ee3106c8d23
Alyu NixOS <alejandro.yusesc@gmail.com>
2024-03-15 19:53:41 +0100

    Configuring hop nvim

--- hosts/laptop/home/conf/neovim/config/lua/alyu/lazy/hop.lua
+++ hosts/laptop/home/conf/neovim/config/lua/alyu/lazy/hop.lua
@@ -2,4 +2,10 @@ return {
     'smoka7/hop.nvim',
     version = "*",
     opts = {},
+    config = function()
+        local hop = require('hop')
+        local directions = require('hop.hint').HintDirection
+		vim.keymap.set('n', '<leader><leader>s', hop.hint_anywere(), { desc = "Hop anywhere" })
+		vim.keymap.set('n', '<leader><leader>l', hop.hint_anywere(), { desc = "Hop line" })
+    end
 }
