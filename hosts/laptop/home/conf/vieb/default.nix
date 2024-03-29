51c3c65ce015aee8e92ed197c231e38ae6a46c1b
Alyu NixOS <alejandro.yusesc@gmail.com>
2024-03-30 00:01:32 +0100

    Setting up vieb

--- hosts/laptop/home/conf/vieb/default.nix
+++ hosts/laptop/home/conf/vieb/default.nix
@@ -1,9 +1,17 @@
 { config, pkgs, lib, ... }: 
 
 {
-    home.file."vieb-config" = {
-        source = ./config;
-        target = ".config/Vieb";
-        recursive = true;
+    # home.file."vieb-config" = {
+    #     source = ./config;
+    #     target = ".config/Vieb";
+    #     recursive = true;
+    # };
+    
+    home.activation = {
+        viebConfig = ''
+            mkdir -p ${config.home.homeDirectory}/.config/Vieb
+            cp -r --preserve=all ${toString ./config}/* ${config.home.homeDirectory}/.config/Vieb
+            chmod -R +w ${config.home.homeDirectory}/.config/Vieb
+        '';
     };
 }
