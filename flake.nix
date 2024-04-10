18559cb79eedc69fde32a879bf9e7d8cd3586592
Alyu NixOS <alejandro.yusesc@gmail.com>
2024-04-11 00:06:06 +0200

    electron-unwrapped workaround

--- flake.nix
+++ flake.nix
@@ -4,6 +4,48 @@
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
+         nixpkgs = {
+            config.packageOverrides = pkgs: rec {
+              electron_28 = pkgs.electron_28.overrideAttrs
+                (oldAttrs: rec {
+
+                  buildCommand =
+                    let
+                      electron-unwrapped = pkgs.electron_28.passthru.unwrapped.overrideAttrs (oldAttrs: rec {
+                        postPatch = builtins.replaceStrings [ "--exclude='src/third_party/blink/web_tests/*'" ] [ "--exclude='src/third_party/blink/web_tests/*' --exclude='src/content/test/data/*'" ] oldAttrs.postPatch;
+                      });
+                    in
+                    ''
+                      gappsWrapperArgsHook
+                      mkdir -p $out/bin
+                      makeWrapper "${electron-unwrapped}/libexec/electron/electron" "$out/bin/electron" \
+                        "''${gappsWrapperArgs[@]}" \
+                        --set CHROME_DEVEL_SANDBOX $out/libexec/electron/chrome-sandbox
+
+                      ln -s ${electron-unwrapped}/libexec $out/libexec
+                    '';
+                });
+              electron = pkgs.electron.overrideAttrs
+                (oldAttrs: rec {
+                  buildCommand =
+                    let
+                      electron-unwrapped = pkgs.electron.passthru.unwrapped.overrideAttrs (oldAttrs: rec {
+                        postPatch = builtins.replaceStrings [ "--exclude='src/third_party/blink/web_tests/*'" ] [ "--exclude='src/third_party/blink/web_tests/*' --exclude='src/content/test/data/*'" ] oldAttrs.postPatch;
+                      });
+                    in
+                    ''
+                      gappsWrapperArgsHook
+                      mkdir -p $out/bin
+                      makeWrapper "${electron-unwrapped}/libexec/electron/electron" "$out/bin/electron" \
+                        "''${gappsWrapperArgs[@]}" \
+                        --set CHROME_DEVEL_SANDBOX $out/libexec/electron/chrome-sandbox
+
+                      ln -s ${electron-unwrapped}/libexec $out/libexec
+                    '';
+                });
+            };
+          };
+
         hyprland.url = "github:hyprwm/Hyprland";
 
         home-manager = {
@@ -23,45 +65,6 @@
     pkgs = import nixpkgs {
         inherit system;
         config.allowUnfree = true;
-        config.packageOverrides = pkgs: rec {
-            electron_28 = pkgs.electron_28.overrideAttrs
-                (oldAttrs: rec {
-
-                 buildCommand =
-                 let
-                 electron-unwrapped = pkgs.electron_28.passthru.unwrapped.overrideAttrs (oldAttrs: rec {
-                         postPatch = builtins.replaceStrings [ "--exclude='src/third_party/blink/web_tests/*'" ] [ "--exclude='src/third_party/blink/web_tests/*' --exclude='src/content/test/data/*'" ] oldAttrs.postPatch;
-                         });
-                 in
-                 ''
-                 gappsWrapperArgsHook
-                 mkdir -p $out/bin
-                 makeWrapper "${electron-unwrapped}/libexec/electron/electron" "$out/bin/electron" \
-                 "''${gappsWrapperArgs[@]}" \
-                 --set CHROME_DEVEL_SANDBOX $out/libexec/electron/chrome-sandbox
-
-                 ln -s ${electron-unwrapped}/libexec $out/libexec
-                 '';
-                 });
-            electron = pkgs.electron.overrideAttrs
-                (oldAttrs: rec {
-                 buildCommand =
-                 let
-                 electron-unwrapped = pkgs.electron.passthru.unwrapped.overrideAttrs (oldAttrs: rec {
-                         postPatch = builtins.replaceStrings [ "--exclude='src/third_party/blink/web_tests/*'" ] [ "--exclude='src/third_party/blink/web_tests/*' --exclude='src/content/test/data/*'" ] oldAttrs.postPatch;
-                         });
-                 in
-                 ''
-                 gappsWrapperArgsHook
-                 mkdir -p $out/bin
-                 makeWrapper "${electron-unwrapped}/libexec/electron/electron" "$out/bin/electron" \
-                 "''${gappsWrapperArgs[@]}" \
-                 --set CHROME_DEVEL_SANDBOX $out/libexec/electron/chrome-sandbox
-
-                 ln -s ${electron-unwrapped}/libexec $out/libexec
-                 '';
-                 });
-        };
     };
     in
     {
