c5f65a60b93ff7bcde907145d060937c6359590e
Alyu NixOS <alejandro.yusesc@gmail.com>
2024-04-11 19:13:38 +0200

    electron-unwrapped workaround

--- flake.nix
+++ flake.nix
@@ -22,48 +22,7 @@
     system = "x86_64-linux";
     pkgs = import nixpkgs {
         inherit system;
-        config = {
-            allowUnfree = true;
-            packageOverrides = pkgs: rec {
-                electron_28 = pkgs.electron_28.overrideAttrs
-                (oldAttrs: rec {
-
-                  buildCommand =
-                    let
-                      electron-unwrapped = pkgs.electron_28.passthru.unwrapped.overrideAttrs (oldAttrs: rec {
-                        postPatch = builtins.replaceStrings [ "--exclude='src/third_party/blink/web_tests/*'" ] [ "--exclude='src/third_party/blink/web_tests/*' --exclude='src/content/test/data/*'" ] oldAttrs.postPatch;
-                      });
-                    in
-                    ''
-                      gappsWrapperArgsHook
-                      mkdir -p $out/bin
-                      makeWrapper "${electron-unwrapped}/libexec/electron/electron" "$out/bin/electron" \
-                        "''${gappsWrapperArgs[@]}" \
-                        --set CHROME_DEVEL_SANDBOX $out/libexec/electron/chrome-sandbox
-
-                      ln -s ${electron-unwrapped}/libexec $out/libexec
-                    '';
-                });
-                electron = pkgs.electron.overrideAttrs
-                (oldAttrs: rec {
-                  buildCommand =
-                    let
-                      electron-unwrapped = pkgs.electron.passthru.unwrapped.overrideAttrs (oldAttrs: rec {
-                        postPatch = builtins.replaceStrings [ "--exclude='src/third_party/blink/web_tests/*'" ] [ "--exclude='src/third_party/blink/web_tests/*' --exclude='src/content/test/data/*'" ] oldAttrs.postPatch;
-                      });
-                    in
-                    ''
-                      gappsWrapperArgsHook
-                      mkdir -p $out/bin
-                      makeWrapper "${electron-unwrapped}/libexec/electron/electron" "$out/bin/electron" \
-                        "''${gappsWrapperArgs[@]}" \
-                        --set CHROME_DEVEL_SANDBOX $out/libexec/electron/chrome-sandbox
-
-                      ln -s ${electron-unwrapped}/libexec $out/libexec
-                    '';
-                });
-            };
-        };
+        config.allowUnfree = true;
     };
     in
     {
@@ -75,10 +34,12 @@
                     ./hosts/laptop/nixos/configuration.nix
                         home-manager.nixosModules.home-manager
                         {
-                            home-manager.useGlobalPkgs = true;
-                            home-manager.useUserPackages = true;
-                            home-manager.users.alyu = import ./hosts/laptop/home/home.nix ;
-                            home-manager.extraSpecialArgs = { inherit inputs outputs system; };
+                            home-manager = {
+                                useGlobalPkgs = true;
+                                useUserPackages = true;
+                                users.alyu = import ./hosts/laptop/home/home.nix ;
+                                extraSpecialArgs = { inherit inputs outputs system; };
+                            };
                         }
                 ];
             };
