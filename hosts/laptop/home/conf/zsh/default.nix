c45f5c1bb9aea8137f26dbbd049ae6cb2794f4f2
Alyu NixOS <alejandro.yusesc@gmail.com>
2024-04-28 23:01:19 +0200

    Prompt with bg

--- hosts/laptop/home/conf/zsh/default.nix
+++ hosts/laptop/home/conf/zsh/default.nix
@@ -79,6 +79,8 @@
                 add_newline = false;
 
                 format = lib.concatStrings [
+                    "$os"
+                    "$username"
                     "$directory"
                     "$cmd_duration"
                     "$line_break"
@@ -96,7 +98,9 @@
                 palettes.gruvbox = {
                     color_fg = "#EBDBB2";
                     color_bg = "#282828";
+                    color_bg3 = "#665c54";
                     color_aqua = "#8EC07C";
+                    color_aqua_dark = "#689D6A";
                     color_red = "#FB4934";
                     color_green = "#B8BB26";
                     color_green_dark = "#98971A";
@@ -113,47 +117,47 @@
 
                 line_break.disabled = false;
 
-                cmd_duration = {
-                    style = "fg:color_blue";
-                    format = "[󰔟 $duration]($style)";
-                };
-
                 os = {
                     disabled = false;
                     symbols.NixOS = " ";
-                    style = "fg:color_blue";
+                    style = "fg:color_bg bg:color_green_dark";
                     format = "[$symbol]($style)";
                 };
 
+                username = {
+                    show_always = true;
+                    style_user = "fg:color_bg bg:color_green_dark";
+                    style_root = "fg:color_red";
+                    format = "[$user ]($style)";
+                };
+
                 directory = {
-                    style = "fg:color_green_dark";
+                    style = "fg:color_bg bg:color_aqua_dark";
                     format = "[ $path ]($style)";
                     truncation_length = 3;
                     truncation_symbol = ".../";
                 };
                 
-                username = {
-                    show_always = true;
-                    style_user = "fg:color_yellow";
-                    style_root = "fg:color_red";
-                    format = "[$user ]($style)";
+                cmd_duration = {
+                    style = "fg:color_bg bg:color_bg3";
+                    format = "[󰔟 $duration]($style)";
                 };
 
                 git_branch = {
                     symbol = "";
-                    style = "fg:color_green";
+                    style = "fg:color_bg bg:color_aqua_dark";
                     format = "[ $symbol $branch ]($style)";
                 };
 
                 git_status = {
-                    style = "fg:color_green";
+                    style = "fg:color_bg bg:color_aqua_dark";
                     format = "[$all_status$ahead_behind]($style)";
                 };
 
                 time = {
                     disabled = false;
                     time_format = "%R";
-                    style = "fg:color_yellow";
+                    style = "fg:color_bg bg:color_green_dark";
                     format = "[ $time ]($style)";
                 };
             };
