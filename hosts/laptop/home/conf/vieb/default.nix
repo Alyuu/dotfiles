b4e18810e6d596b27488531856c03b4b137c357e
Alyu NixOS <alejandro.yusesc@gmail.com>
2024-05-05 12:48:01 +0200

    Changed vieb .desktop

--- hosts/laptop/home/conf/vieb/default.nix
+++ hosts/laptop/home/conf/vieb/default.nix
@@ -14,4 +14,16 @@
             chmod -R +w ${config.home.homeDirectory}/.config/Vieb
         '';
     };
+
+    xdg.desktopEntries = {
+        vieb = {
+            categories = [ "Application" "Network" "WebBrowser" ];
+            exec = "vieb %U";
+            genericName = "Web Browser";
+            icon = "vieb";
+            mimeType = [ "text/html" "application/xhtml+xml" "x-scheme-handler/https" ];
+            name = "Vieb";
+            type = "Application";
+        };
+    };
 }
