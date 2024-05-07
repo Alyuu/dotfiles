dd481130d37fe7167bff6d05af31f4afa0415917
Alyu NixOS <alejandro.yusesc@gmail.com>
2024-05-07 04:30:25 +0200

    hotfix hyprland

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-        hyprland.url = "github:hyprwm/Hyprland";
+        hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
 
         home-manager = {
             url = "github:nix-community/home-manager";
