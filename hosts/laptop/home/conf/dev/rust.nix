1c8f2a78204fd6b03d83704167f5e9b57f4c39a1
Alyu NixOS <alejandro.yusesc@gmail.com>
2024-05-14 00:37:07 +0200

    shurely now

--- hosts/laptop/home/conf/dev/rust.nix
+++ hosts/laptop/home/conf/dev/rust.nix
@@ -17,7 +17,7 @@
         };
     };
 
-    programs.zsh.variables = {
+    programs.zsh.sessionVariables = {
         RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
         RUST_BACTRACE = 1;
         PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
