73461d2450deb51c5ebb689bed5893fc80a6f915
Alyu NixOS <alejandro.yusesc@gmail.com>
2024-05-14 00:33:50 +0200

    now?

--- hosts/laptop/home/conf/dev/rust.nix
+++ hosts/laptop/home/conf/dev/rust.nix
@@ -14,6 +14,9 @@
 
         sessionVariables = {
             CARGO_HOME = "${config.xdg.dataHome}/cargo";
+        };
+
+        programs.zsh.variables = {
             RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
             RUST_BACTRACE = 1;
             PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
