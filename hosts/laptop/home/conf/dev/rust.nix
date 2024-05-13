c8061a2b9294d0be73930a4e4917b37aeeadbf7f
Alyu NixOS <alejandro.yusesc@gmail.com>
2024-05-14 00:14:47 +0200

    rust openssl fix

--- hosts/laptop/home/conf/dev/rust.nix
+++ hosts/laptop/home/conf/dev/rust.nix
@@ -16,6 +16,7 @@
             CARGO_HOME = "${config.xdg.dataHome}/cargo";
             RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
             RUST_BACTRACE = 1;
+            PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
         };
     };
 }
