5ce4c4afa47fbaac01bc4dae4e9715eea7c8bb4e
Alyu NixOS <alejandro.yusesc@gmail.com>
2024-05-13 23:08:13 +0200

    Added pkg-config and openssl

--- hosts/laptop/home/conf/dev/rust.nix
+++ hosts/laptop/home/conf/dev/rust.nix
@@ -8,6 +8,8 @@
             rustc
             rustfmt
             sqlx-cli
+            pkg-config
+            openssl
         ];
 
         sessionVariables.CARGO_HOME = "${config.xdg.dataHome}/cargo";
