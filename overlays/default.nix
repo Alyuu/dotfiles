03a63b988ae9e334d9659210ac53efd5c597a80f
Alyuu <alejandro.yusesc@gmail.com>
2024-08-17 22:49:15 +0200

    no overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,15 +1,3 @@
 { inputs, ... }:
 {
-    modifications = final: prev: {
-        delta = prev.delta.overrideAttrs (old: {
-            version = "0.18.0";
-            src = prev.fetchFromGitHub {
-                owner = "dandavison";
-                repo = "delta";
-                rev = "refs/tags/0.18.0";
-                hash = "sha256-1UOVRAceZ4QlwrHWqN7YI2bMyuhwLnxJWpfyaHNNLYg=";
-            };
-            cargoHash = "sha256-/h7djtaTm799gjNrC6vKulwwuvrTHjlsEXbK2lDH+rc=";
-        });
-    };
 }
