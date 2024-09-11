7afd02bb7d871fc5e1630e8b16f73d5e24403be5
Alyuu <alejandro.yusesc@gmail.com>
2024-09-11 19:10:34 +0200

    tmux things

--- home/alyu/common/core/tmux.nix
+++ home/alyu/common/core/tmux.nix
@@ -1,9 +1,38 @@
 { pkgs, ... }:
+let
+    tmux-gruvbox = pkgs.tmuxPlugins.mkTmuxPlugin
+        {
+            pluginName = "tmux-gruvbox";
+            version = "unstable-2024-09-11";
+            src = pkgs.fetchFromGitHub {
+                owner = "Alyuu";
+                repo = "tmux-gruvbox";
+                rev = "61ec5d5f634d09dbf1570bb436d1e7a723105470";
+                sha256 = "";
+            };
+        };
+in
 {
     programs.tmux = {
         enable = true;
         shell = "${pkgs.zsh}/bin/zsh";
+        terminal = "kitty";
+        shortcut = "a";
+        keyMode = "vi";
+        mouse = true;
         clock24 = true;
         historyLimit = 10000;
+        plugins = with pkgs; [
+            {
+                package = tmux-gruvbox;
+            }
+        ];
+        extraConfig = ''
+            set-option -ga terminal-overrides ",kitty:Tc"
+            bind -n M-Left select-pane -L
+            bind -n M-Right select-pane -R
+            bind -n M-Up select-pane -U
+            bind -n M-Down select-pane -D
+        '';
     };
 }
